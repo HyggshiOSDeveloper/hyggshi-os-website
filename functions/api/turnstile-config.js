fdf0dcfd0fe527775dffb85377c3644bef268d70
Hyggshi OS <tfhy5321@gmail.com>
2026-05-13 12:42:58 +0700

    Delete functions/api/turnstile-config.js

--- functions/api/turnstile-config.js
+++
@@ -1,9 +0,0 @@
-export async function onRequestGet({ env }) {
-    return Response.json({
-        siteKey: env.GC_TURNSTILE_SITE_KEY || env.TURNSTILE_SITE_KEY || ''
-    }, {
-        headers: {
-            'Cache-Control': 'no-store'
-        }
-    });
-}
